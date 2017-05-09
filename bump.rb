094f1ea965d067d8a704a02f90c390bb964b99c8
taicsuzu <taicsuzu@yahoo-corp.jp>
2017-05-09 17:13:34 +0900

    update

--- bump.rb
+++ bump.rb
@@ -1,15 +1,23 @@
 def bump_version_cr(version)
+  file_path = File.expand_path("../src/neph/version.cr", __FILE__)
+
   puts "Bump the version for version.cr"
-  file = File.read(File.expand_path("../src/neph/version.cr", __FILE__))
+  file = File.read(file_path)
   file.sub!(/VERSION = "(.+)"/, "VERSION = \"#{version}\"")
-  File.write(File.expand_path("../src/neph/version.cr", __FILE__), file)
+  File.write(file_path, file)
+
+  `git add #{file_path}`
 end
 
 def bump_shard_yml(version)
+  File.expand_path("../shard.yml", __FILE__)
+  
   puts "Bump the version for shard.yml"
-  file = File.read(File.expand_path("../shard.yml", __FILE__))
+  file = File.read(file_path)
   file.sub!(/version: (.+)/, "version: #{version}")
-  File.write(File.expand_path("../shard.yml", __FILE__), file)
+  File.write(file_path, file)
+
+  `git add #{file_path}`
 end
 
 abort("Failed to file a bumping version") if ARGV.size.zero?
